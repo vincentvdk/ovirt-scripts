e84a073ede5535c81449030868fd654ad8221d19
Jacob Castello <jacob@arin.net>
2015-11-06 13:38:20 -0500

    adding config file, boot order, and hidden password

--- create-vm.py
+++ create-vm.py
@@ -21,7 +21,9 @@ import getopt
 import optparse
 import os
 import time
-
+import json
+import getpass
+import ConfigParser
 
 from ovirtsdk.api import API
 from ovirtsdk.xml import params
@@ -37,59 +39,42 @@ osver can be: rhel_6x64, etc
 """
 
 # Option parsing
-p = optparse.OptionParser("rhev-vm-create.py [arguments]", description=description)
-p.add_option("-u", "--user", dest="username", help="Username to connect to ovirt-engine API", metavar="admin@internal", default="admin@internal")
-p.add_option("-w", "--password", dest="password", help="Password to use with username", metavar="admin", default="redhat")
-p.add_option("-s", "--server", dest="server", help="RHEV-M server address/hostname to contact", metavar="server", default="127.0.0.1")
-p.add_option('-v', "--verbosity", dest="verbosity", help="Show messages while running", metavar='[0-n]', default=0, type='int')
-p.add_option("-n", "--name", dest="name", help="VM name", metavar="name", default="name")
-p.add_option("-c", "--cluster", dest="cluster", help="VM cluster", metavar="cluster", default="Default")
-p.add_option("--vmcpu", dest="vmcpu", help="VM CPU", metavar="vmcpu", default="1")
-p.add_option("--vmmem", dest="vmmem", help="VM RAM in GB", metavar="vmmem", default="1")
-p.add_option("--sdtype", dest="sdtype", help="SD type", metavar="sdtype", default="Default")
-p.add_option("--sdsize", dest="sdsize", help="SD size in GB", metavar="sdsize", default="20")
-p.add_option("--osver", dest="osver", help="OS version", metavar="osver", default="rhel_6x64")
-p.add_option("--vmnet", dest="vmnet", help="Network to use", metavar="vmnet", default="rhevm")
+def getParser(defaults):
+    p = optparse.OptionParser("rhev-vm-create.py [arguments]", description=description)
+    p.set_defaults(**defaults)
+    p.add_option("-u", "--username", dest="username", help="Username to connect to ovirt-engine API", metavar="admin@internal")
+    p.add_option("-w", "--password", dest="password", help="Password to use with username", metavar="admin")
+    p.add_option("-s", "--server", dest="server", help="RHEV-M server address/hostname to contact", metavar="server")
+    p.add_option('-v', "--verbosity", dest="verbosity", help="Show messages while running", metavar='[0-n]', type='int')
+    p.add_option("-n", "--name", dest="name", help="VM name", metavar="name")
+    p.add_option("-c", "--cluster", dest="cluster", help="VM cluster", metavar="cluster")
+    p.add_option("--vmcpu", dest="vmcpu", help="VM CPU", metavar="vmcpu")
+    p.add_option("--vmmem", dest="vmmem", help="VM RAM in GB", metavar="vmmem")
+    p.add_option("--sdtype", dest="sdtype", help="SD type", metavar="sdtype")
+    p.add_option("--sdsize", dest="sdsize", help="SD size in GB", metavar="sdsize")
+    p.add_option("--osver", dest="osver", help="OS version", metavar="osver")
+    p.add_option("--vmnet", dest="vmnet", help="Network to use", metavar="vmnet")
+    p.add_option("--config", dest="config", help="JSON config file")
+    p.add_option("--storage", dest="storage_name", help="Name of the storage domain")
+    p.add_option("--ca", dest="ca_file", help="Path to the ca file")
+    p.add_option("--insecure", dest="insecure", help="Connect without validating the CA")
+    
+    return p
 
-(options, args) = p.parse_args()
-
-baseurl = "https://%s" % (options.server)
-
-api = API(url=baseurl, username=options.username, password=options.password, insecure=True)
-
-try:
-    value = api.hosts.list()
-except:
-    print "Error accessing RHEV-M api, please check data and connection and retry"
-    sys.exit(1)
-
-# Define VM based on parameters
-if __name__ == "__main__":
-    vmparams = params.VM(os=params.OperatingSystem(type_=options.osver),
-    cpu=params.CPU(topology=params.CpuTopology(cores=int(options.vmcpu))),
-    name=options.name, memory=1024 * 1024 * 1024 * int(options.vmmem),
-    cluster=api.clusters.get(name=options.cluster),
-    template=api.templates.get(name="Blank"), type_="server")
-    vmdisk = params.Disk(size=1024 * 1024 * 1024 * int(options.sdsize), wipe_after_delete=True, sparse=True, interface="virtio", type_="System", format="cow",
-    storage_domains=params.StorageDomains(storage_domain=[api.storagedomains.get(name="DATA1")]))
-    vmnet = params.NIC()
-
-    network_net = params.Network(name=options.vmnet)
-
-    nic_net1 = params.NIC(name='nic1', network=network_net, interface='virtio')
-
+# Define the function to add vms
+def add_vm(vmparams, name, vmdisk, nic_net1):
     try:
         api.vms.add(vmparams)
     except:
         print "Error creating VM with specified parameters, recheck"
         sys.exit(1)
 
-    if options.verbosity > 1:
+    if verbosity > 1:
         print "VM created successfuly"
 
-    if options.verbosity > 1:
+    if verbosity > 1:
         print "Attaching networks and boot order..."
-    vm = api.vms.get(name=options.name)
+    vm = api.vms.get(name=name)
     vm.nics.add(nic_net1)
 
     try:
@@ -98,26 +83,112 @@ if __name__ == "__main__":
         print "Error attaching networks, please recheck and remove configurations left behind"
         sys.exit(1)
 
-    if options.verbosity > 1:
+    if verbosity > 1:
         print "Adding HDD"
     try:
         vm.disks.add(vmdisk)
     except:
         print "Error attaching disk, please recheck and remove any leftover configuration"
-
-    if options.verbosity > 1:
+        sys.ext(1)
+    
+    if verbosity > 1:
+        print "Setting boot order"
+    try:
+        hd_boot_dev = params.Boot(dev='hd')
+        net_boot_dev = params.Boot(dev='network')
+        vm.os.set_boot([net_boot_dev, hd_boot_dev])
+    except:
+        print "Error setting boot order"
+        sys.exit(1)
+    
+    if verbosity > 1:
         print "VM creation successful"
 
-    vm = api.vms.get(name=options.name)
+    vm = api.vms.get(name=name)
     vm.high_availability.enabled = True
     vm.update()
 
-#wait until VM is stopped before we start it.
-status = api.vms.get(name=options.name).status.state
-while status != 'down':
-		print status
-		time.sleep(5)
-		status = api.vms.get(name=options.name).status.state
-		vm.start()
+    #wait until VM is stopped before we start it.
+    status = api.vms.get(name=name).status.state
+    while status != 'down':
+        print status
+        time.sleep(1)
+        status = api.vms.get(name=name).status.state
+    vm.start()
 
+# Define VM based on parameters
+if __name__ == "__main__":
+   
+    my_defaults = {'verbose': 1}
+    options, args = getParser(my_defaults).parse_args()
+    if options.config is not None:
+        configToLoad = options.config
+    else:
+        configToLoad = None
+
+    if configToLoad is not None:
+        loadedConfig = json.load(open(configToLoad))
+        my_defaults.update(loadedConfig)
+        options, args = getParser(my_defaults).parse_args()
+
+    username = options.username
+    server = options.server
+    name = options.name
+    cluster = options.cluster
+    vmcpu = options.vmcpu
+    vmmem = options.vmmem
+    sdtype = options.sdtype
+    sdsize = options.sdsize
+    osver = options.osver
+    vmnet = options.vmnet
+    verbosity = options.verbosity
+    storage_name = options.storage_name
+    ca_file = options.ca_file
+    insecure = options.insecure
+
+    if options.password is None:
+        password = getpass.getpass()
+    else:
+        password = options.password
+
+    baseurl = "https://%s" % (server)
+    
+    if options.insecure:
+        try:
+            api = API(url=baseurl, username=username, password=password, insecure=True),
+        except Exception, e:
+            print "Unable to make API connection:\n %s" % str(e)
+            sys.exit(1)
+    else:
+        try:
+            api = API(url=baseurl, username=username, password=password, ca_file=ca_file)
+        except Exception, e:
+           print "Unable to make API connection:\n %s" % str(e)
+           sys.exit(1)
+    
+    
+    # Set the parameters for VM creation
+    vmparams = params.VM(os=params.OperatingSystem(type_=osver),
+            cpu=params.CPU(topology=params.CpuTopology(cores=int(vmcpu))),
+            name=name, 
+            memory=1024 * 1024 * 1024 * int(vmmem),
+            cluster=api.clusters.get(name=cluster),
+            template=api.templates.get(name="Blank"), type_="server")
+    
+    vmdisk = params.Disk(
+            size=1024 * 1024 * 1024 * int(sdsize), 
+            wipe_after_delete=True, 
+            sparse=True, 
+            interface="virtio", 
+            type_="System", 
+            bootable=True,
+            format="cow",
+            storage_domains=params.StorageDomains(storage_domain=[api.storagedomains.get(name=storage_name)]))
+
+    #vmnet = params.NIC()
+    network_net = params.Network(name=vmnet)
+    nic_net1 = params.NIC(name='nic1', network=network_net, interface='virtio')
+    
+    # Commit the action of adding the VM
+    add_vm(vmparams, name, vmdisk, nic_net1)
 #print "MAC:%s" % vm.nics.get(name="eth0").mac.get_address()
